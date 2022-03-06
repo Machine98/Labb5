84f8a2556c93f5aa23a3ea85a4fac8b2f6444511
Machine98 <sunali-1@student.ltu.se>
2022-03-06 15:55:03 +0100

    CQ

--- lab5/general/store/CustomerQueue.java
+++ lab5/general/store/CustomerQueue.java
@@ -2,5 +2,11 @@ package lab5.general.store;
 
 import lab5.general.FIFO;
 
-public class CustomerQueue  {
+public class CustomerQueue {
+
+    private FIFO customerFIFO;
+
+    public CustomerQueue() {
+        this.customerFIFO = new FIFO();
+    }
 }
