fb12562217a0a3feb0abf01e299ad3a036bbcb78
EricVikberg <ericvikberg@gmail.com>
2022-03-06 15:52:26 +0100

    View

--- lab5/general/View.java
+++ lab5/general/View.java
@@ -1,4 +1,17 @@
 package lab5.general;
 
-public class View {
+import java.util.Observer;
+import java.util.Observable;
+import lab5.general.store.StoreState;
+
+public class View implements Observer{
+
+    private State state;
+    public View() {
+        state.addObserver(this);
+    }
+
+    public void update(Observable arg, object o); {
+
+    }
 }
