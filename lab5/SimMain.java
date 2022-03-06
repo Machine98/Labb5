5675aa0dbe0f173b98b46a08ad770f8c1d091887
themooses <wilhelm.rauston@gmail.com>
2022-03-06 14:52:34 +0100

    SimMain structure

--- lab5/SimMain.java
+++ lab5/SimMain.java
@@ -1,4 +1,31 @@
 package lab5;
 
+
+import lab5.general.Simulator;
+import lab5.general.View;
+import lab5.general.store.StoreSate;
+import lab5.general.store.StoreView;
+
+
 public class SimMain {
+
+    public static void main(String[] args) {
+        long seed = 1234;
+        int maxCustomers = 10;
+        int registers = 4;
+        double closingTime = 1000;
+        double minPickTime = 0.5;
+        double maxPickTime = 1;
+        double minPayTime = 2;
+        double maxPayTime = 3;
+        double lambda = 1;
+
+
+        StoreSate state = new StoreSate(seed, maxCustomers, registers, closingTime, minPayTime, maxPayTime, minPickTime, maxPickTime, lambda);
+        StoreView view = new View(state, state.getStore());
+
+        Simulator simulator = new Simulator(state, view);
+
+
+    }
 }
