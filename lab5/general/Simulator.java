3a7c0b0e8f97a7ecb2bf513c36bc233aa85a1729
themooses <wilhelm.rauston@gmail.com>
2022-03-08 14:00:42 +0100

    SimMain structure & State structure

--- lab5/general/Simulator.java
+++ lab5/general/Simulator.java
@@ -1,5 +1,7 @@
 package lab5.general;
 
+import lab5.general.store.*;
+
 public class Simulator {
 
     private State state;
@@ -13,7 +15,16 @@ public class Simulator {
     }
 
     public void run() {
+        eventQueue.addEvent(new StartEvent(state, eventQueue));
+        eventQueue.addEvent(new EndEvent(state, eventQueue));
+    }
 
 
+    private void simulating() {
+
+        while (eventQueue.getNext() && state.getRunningValue() == true){
+            eventQueue.run();
+        }
     }
 }
+
