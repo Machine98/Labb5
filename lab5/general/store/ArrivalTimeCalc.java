07ebe6f48ddee144d238c35df8a3fead66dd1b9f
EricVikberg <ericvikberg@gmail.com>
2022-03-10 19:51:11 +0100

    ArrivalTimeCalc

--- lab5/general/store/ArrivalTimeCalc.java
+++ lab5/general/store/ArrivalTimeCalc.java
@@ -1,4 +1,19 @@
 package lab5.general.store;
 
+import lab5.ExponentialRandomStream;
+
 public class ArrivalTimeCalc {
+    private double lambda;
+    private long seed;
+    private ExponentialRandomStream ArrivalTime;
+
+    public ArrivalTimeCalc(double lambda, long seed) {
+        this.lambda = lambda;
+        this.seed = seed;
+        this.ArrivalTime = new ExponentialRandomStream(lambda, seed);
+    }
+
+    public double newArrivalTime() {
+        return ArrivalTime.next();
+    }
 }
