229b9d163dfe4e283b690dff0f95365743457154
Machine98 <sunali-1@student.ltu.se>
2022-03-11 15:45:07 +0100

    Optimize

--- lab5/Optimize.java
+++ lab5/Optimize.java
@@ -1,4 +1,42 @@
 package lab5;
 
+import lab5.general.EventQueue;
+import lab5.general.Simulator;
+import lab5.general.View;
+import lab5.general.store.*;
+
 public class Optimize {
+
+    public void metod1(){
+        int registers = 2;
+        int maxCustomers = 5;
+        double lambda = 0.5;
+        double minPickTime = 1.0;
+        double maxPickTime = 2.0;
+        double minPayTime = 3.0;
+        double maxPayTime = 10;
+        long seed = 1234;
+
+        StoreState storeState = new StoreState(seed, maxCustomers, registers, minPayTime, maxPayTime, minPickTime, maxPickTime, lambda);
+
+        EventQueue eventQueue = new EventQueue();
+
+        eventQueue.addEvent(new StartEvent(storeState, 0, eventQueue));
+        eventQueue.addEvent(new ClosingEvent(storeState, 99999, eventQueue));
+        eventQueue.addEvent(new EndEvent(storeState, 999999, eventQueue));
+
+        //View view = new StoreView(storeState);
+        //storeState.addObserver(view);
+
+        //Simulator simulator = new Simulator(storeState, view, eventQueue);
+        //simulator.run();
+    }
+
+    public void metod2(){
+
+    }
+
+    public void metod3(){
+
+    }
 }
