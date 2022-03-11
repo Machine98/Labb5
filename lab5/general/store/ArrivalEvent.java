0634dccbd4ca94a6f877caf3df7058d32ef5ff03
FilthyTv <64787614+FilthyTv@users.noreply.github.com>
2022-03-11 15:44:18 +0100

    getname i event2

--- lab5/general/store/ArrivalEvent.java
+++ lab5/general/store/ArrivalEvent.java
@@ -7,25 +7,24 @@ import lab5.general.State;
 public class ArrivalEvent extends Event {
     private double time;
     private EventQueue eventQueue;
+    private State state;
     private boolean open;
     private StoreState storeState;
     private Customer customerID;
     private Event event;
-    private State state;
 
-    public ArrivalEvent(StoreState storeState, double time, EventQueue eventQueue) {
-        super(storeState, time, eventQueue);
-
+    public ArrivalEvent(State state, double time, EventQueue eventQueue) {
+        super((StoreState) state, time, eventQueue);
     }
 
     @Override
     public void performEvent() {
-        double newTime = ArrivalTimeCalc.newArrivalTime();
+        time = ArrivalTimeCalc.newArrivalTime();
         if(event.getIsOpen()){
             if(storeState.getCurrentCustomers() == storeState.getMaxCustomers()){
                 storeState.setCustomersTurnedAway(1);
             }else{
-                eventQueue.addEvent(new PickUpEvent(storeState, newTime, customerID, eventQueue, state));
+                eventQueue.addEvent(new PickUpEvent(storeState, time, customerID, eventQueue, state));
             }
         }
     }
