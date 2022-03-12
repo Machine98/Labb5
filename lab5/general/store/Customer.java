172b6877980553ab94240a717157893b37a4602d
Machine98 <sunali-1@student.ltu.se>
2022-03-12 17:58:46 +0100

    Error Fix

--- lab5/general/store/Customer.java
+++ lab5/general/store/Customer.java
@@ -3,24 +3,15 @@ package lab5.general.store;
 public class Customer {
 
     private int customerID;
-    private double pickTime;
-    private double payTime;
+
 
     public Customer(int id, StoreState storeState){
-        this.pickTime = storeState.getPickTime();
-        this.payTime = storeState.getPayTime();
         setCustomerID(id);
     }
 
     public void setCustomerID(int id){
 
         this.customerID = id;
-    }
-    public double getCustomerPickTime() {
-        return pickTime;
-    }
-    public double getCustomerPayTime() {
-        return payTime;
     }
     public int getCustomerID(){
 
