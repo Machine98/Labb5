5512c9bf2d8abbb7910669674744be148ad0dd11
EricVikberg <ericvikberg@gmail.com>
2022-03-13 21:17:56 +0100

    Optimize och K

--- lab5/Optimize.java
+++ lab5/Optimize.java
@@ -9,15 +9,33 @@ import lab5.general.K;
 import java.awt.*;
 import java.util.Random;
 
+/**
+ * Represents the optimization of the number of cash registers.
+ *
+ * @author Calle Rautio
+ * @author Wilhelm Rauston
+ * @author Albin Sundström
+ * @author Eric Vikberg
+ */
+
+
 public class Optimize implements K{
-
+    /**
+     * Make calls to the optimization methods. (?)
+     *
+     */
     public static void main(String[] args) {
-
 
         //metod1(2,SEED);
         metod2(SEED);
         //metod3(SEED);
     }
+    /**
+     * Runs the simulation, without printing any data, and returns the final state.
+     *
+     *
+     * @param seed - The seed that is sent as an argument to metod1().
+     */
     private static StoreState metod1(int registers, long seed){
 
         StoreState storeState = new StoreState(seed, M, registers, LOW_COLLECTION_TIME, HIGH_COLLECTION_TIME,
@@ -37,12 +55,18 @@ public class Optimize implements K{
         //System.out.println(storeState.getCustomersTurnedAway());
         return storeState;
     }
-    static int metod2(long seed) {
+    /**
+     * Finds the lowest number of registers that minimizes the number of
+     * missed customers for the given seed.
+     *
+     * @param seed - The seed that is sent as an argument to metod1().
+     */
+    private static int metod2(long seed) {
         StoreState newState;
 
         int optimalAmOfReg = 0;
 
-        int missed = 999999;
+        int missed = 99999;
         for (int i = M; i >= 1; i--) {
             newState = metod1(i,seed);
             //System.out.println(newState.getCustomersTurnedAway());
@@ -52,11 +76,16 @@ public class Optimize implements K{
             missed = newState.getCustomersTurnedAway();
             optimalAmOfReg = i;
         }
-        //printjävel();
+
         System.out.println("Minst antal kassor: "+"("+missed+") "+optimalAmOfReg);
         return optimalAmOfReg;
     }
-
+    /**
+     * Test different seeds to find the highest return value of metod2().
+     *
+     *
+     * @param f - The seed to the random number generator, Random(f).
+     */
     private static void metod3(long f) {
         Random randomSeed = new Random(f);
         int loopsSinceChange = 0;
@@ -75,17 +104,8 @@ public class Optimize implements K{
                 break;
             }
         }
-        //printjävel();
         System.out.print("Worst case - minst antal kassor: "+optimalAmOfReg);
 
     }
-    private static void printjävel() {
-        System.out.println("Max som ryms, M..........: "+M);
-        System.out.println("Ankomshastighet, lambda..: "+L);
-        System.out.println("Plocktider, [P_min..Pmax]: "+"["+LOW_COLLECTION_TIME+" .. "+HIGH_COLLECTION_TIME+"]");
-        System.out.println("Plocktider, [P_min..Pmax]: "+"["+LOW_PAYMENT_TIME+" .. "+HIGH_PAYMENT_TIME+"]");
-        System.out.println("Frö, f...................: "+SEED);
-        System.out.println("Stängning sker tiden "+END_TIME+" och stophändelsen sker tiden "+STOP_TIME);
-        System.out.print("Minsta antal kassor som ger minimalt antal missade ");
-    }
+
 }
