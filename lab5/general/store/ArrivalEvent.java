68600369380404b22847dd3aee4f0a491d849299
FilthyTv <64787614+FilthyTv@users.noreply.github.com>
2022-03-13 15:24:17 +0100

    JavaDoc ArrivalEvent

--- lab5/general/store/ArrivalEvent.java
+++ lab5/general/store/ArrivalEvent.java
@@ -3,16 +3,37 @@ package lab5.general.store;
 import lab5.general.Event;
 import lab5.general.EventQueue;
 
+/**
+ * Represents the arrival of a new customer
+ *
+ * @author Calle Rautio
+ * @author Wilhelm Rauston
+ * @author Albin Sundström
+ * @author Eric Vikberg
+ */
+
 public class ArrivalEvent extends Event {
     private double time;
     private Customer customerID;
     private StoreState storeState;
+
+    /**
+     * Constructor
+     *
+     * @param storeState - The stores state that keeps track of all the moving variables
+     * @param time - The time that it takes for an event to be run
+     * @param eventQueue - The event queue where it will add new events
+     */
 
     public ArrivalEvent(StoreState storeState, double time, EventQueue eventQueue) {
         super(storeState, time, eventQueue);
         this.storeState = storeState;
         this.time = time;
     }
+
+    /**
+     * Performs the changes for a customer arriving
+     */
 
     @Override
     public void performEvent() {
