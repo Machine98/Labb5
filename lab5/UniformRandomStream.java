d93b46af3a71872b8e27cb34d507d623f7203dad
themooses <wilhelm.rauston@gmail.com>
2022-03-13 15:02:25 +0100

    UniformRandomStream: Javadoc

--- lab5/UniformRandomStream.java
+++ lab5/UniformRandomStream.java
@@ -3,10 +3,28 @@ package lab5;
 import java.util.Random;
 
 
+/**
+ * Represents a program feature for randomizing time according to simulation conditions
+ *
+ * @author Calle Rautio
+ * @author Wilhelm Rauston
+ * @author Albin Sundström
+ * @author Eric Vikberg
+ */
+
 public class UniformRandomStream {
+
 
     private Random rand;
     private double lower, width;
+
+    /**
+     * Constructor
+     *
+     * @param lower - minimum paytime
+     * @param upper - maximum paytime
+     * @param seed  - a seed used to initialize a pseudorandom number generator
+     */
 
     public UniformRandomStream(double lower, double upper, long seed) {
         rand = new Random(seed);
@@ -14,11 +32,22 @@ public class UniformRandomStream {
         this.width = upper - lower;
     }
 
+    /**
+     * Constructor
+     *
+     * @param lower - minimum paytime
+     * @param upper - maximum paytime
+     */
+
     public UniformRandomStream(double lower, double upper) {
         rand = new Random();
         this.lower = lower;
         this.width = upper - lower;
     }
+
+    /**
+     * @return A random double between lower and upper
+     */
 
     public double next() {
         return lower + rand.nextDouble() * width;
