4dc59d63a7aa2f0ebd20bb20394561bd9030c410
EricVikberg <ericvikberg@gmail.com>
2022-03-13 17:37:57 +0100

    Optimize och K

--- lab5/Optimize.java
+++ lab5/Optimize.java
@@ -4,35 +4,29 @@ import lab5.general.EventQueue;
 import lab5.general.Simulator;
 import lab5.general.View;
 import lab5.general.store.*;
+import lab5.general.K;
 
 import java.awt.*;
 import java.util.Random;
 
-public class Optimize {
+public class Optimize implements K{
 
     public static void main(String[] args) {
 
-        metod1(2,1234);
-        metod2(1234);
-        metod3(1234);
+        metod1(2,SEED);
+        metod2(SEED);
+        metod3(SEED);
     }
     private static StoreState metod1(int registers, long seed){
 
-        int maxCustomers = 5;
-        double lambda = 2.0;
-        double minPickTime = 0.5;
-        double maxPickTime = 1.0;
-        double minPayTime = 2.0;
-        double maxPayTime = 3.0;
-
-
-        StoreState storeState = new StoreState(seed, maxCustomers, registers, minPickTime, maxPickTime, minPayTime, maxPayTime, lambda);
+        StoreState storeState = new StoreState(seed, M, registers, LOW_COLLECTION_TIME, HIGH_COLLECTION_TIME,
+                LOW_PAYMENT_TIME, HIGH_PAYMENT_TIME, L);
 
         EventQueue eventQueue = new EventQueue();
 
         eventQueue.addEvent(new StartEvent(storeState, 0, eventQueue));
-        eventQueue.addEvent(new ClosingEvent(storeState, 10, eventQueue));
-        eventQueue.addEvent(new EndEvent(storeState, 999999, eventQueue));
+        eventQueue.addEvent(new ClosingEvent(storeState, STOP_TIME, eventQueue));
+        eventQueue.addEvent(new EndEvent(storeState, END_TIME, eventQueue));
 
         View view = new StoreView(storeState);
         //storeState.addObserver(view);
@@ -48,7 +42,7 @@ public class Optimize {
         int optimalAmOfReg = 0;
 
         int compare = 999999;
-        for (int i = 5; i >= 1; i--) {
+        for (int i = M; i >= 1; i--) {
             newState = metod1(i,seed);
             //System.out.println(newState.getCustomersTurnedAway());
             if(compare < newState.getCustomersTurnedAway()) {
@@ -57,7 +51,8 @@ public class Optimize {
             compare = newState.getCustomersTurnedAway();
             optimalAmOfReg = i;
         }
-        //System.out.println(optimalAmOfReg);
+        //printjävel();
+        //System.out.print("("+compare+") ");
         return optimalAmOfReg;
     }
 
@@ -79,6 +74,17 @@ public class Optimize {
                 break;
             }
         }
-        System.out.println(optimalAmOfReg);
+        printjävel();
+        System.out.print(optimalAmOfReg);
+
+    }
+    private static void printjävel() {
+        System.out.println("Max som ryms, M..........: "+M);
+        System.out.println("Ankomshastighet, lambda..: "+L);
+        System.out.println("Plocktider, [P_min..Pmax]: "+"["+LOW_COLLECTION_TIME+" .. "+HIGH_COLLECTION_TIME+"]");
+        System.out.println("Plocktider, [P_min..Pmax]: "+"["+LOW_PAYMENT_TIME+" .. "+HIGH_PAYMENT_TIME+"]");
+        System.out.println("Frö, f...................: "+SEED);
+        System.out.println("Stängning sker tiden "+STOP_TIME+" och stophändelsen sker tiden "+END_TIME);
+        System.out.print("Minsta antal kassor som ger minimalt antal missade ");
     }
 }
