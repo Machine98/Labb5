89eac310847c2a27d2967d445be1569231a409eb
Machine98 <sunali-1@student.ltu.se>
2022-03-13 14:55:50 +0100

    Cleanup

--- lab5/general/State.java
+++ lab5/general/State.java
@@ -1,7 +1,5 @@
 package lab5.general;
 
-import lab5.general.store.StoreState;
-
 import java.util.Observable;
 
 public abstract class State extends Observable {
@@ -25,6 +23,4 @@ public abstract class State extends Observable {
     public void setTimePassed(double newTime) {
         this.timePassed = newTime;
     }
-
-
 }
--- lab5/general/View.java
+++ lab5/general/View.java
@@ -3,19 +3,14 @@ package lab5.general;
 import java.util.Observer;
 import java.util.Observable;
 
-import lab5.general.store.StoreState;
-
-public abstract class View implements Observer{
+public abstract class View implements Observer {
 
     private State state;
 
     public View() {
-
-        //state.addObserver(this);
     }
 
     @Override
     public void update(Observable arg0, Object f) {
-
     }
 }
