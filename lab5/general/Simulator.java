e88d2484b235c1a8b015e20e532dec77438b0daf
themooses <wilhelm.rauston@gmail.com>
2022-03-13 15:27:58 +0100

    Simulator: Javadoc

--- lab5/general/Simulator.java
+++ lab5/general/Simulator.java
@@ -2,6 +2,15 @@ package lab5.general;
 
 import lab5.general.store.*;
 
+/**
+ * Represents the programs function of running the simulation, gathering the events stacked in the EventQueue and performing the events and calling the prints
+ *
+ * @author Calle Rautio
+ * @author Wilhelm Rauston
+ * @author Albin Sundström
+ * @author Eric Vikberg
+ */
+
 public class Simulator {
 
     private State state;
@@ -9,11 +18,24 @@ public class Simulator {
     private View view;
     private Event event;
 
+    /**
+     * Constructor
+     *
+     * @param state - the general state of the program
+     * @param view - the view
+     * @param eventQueue  - the queue of events
+     */
+
     public Simulator(State state, View view, EventQueue eventQueue) {
         this.state = state;
         this.view = view;
         this.eventQueue = eventQueue;
     }
+
+    /**
+     * handles performing the events in the queue and calling the prints out of storeview
+     *
+     */
 
     public void run() {
         ((StoreView) view).firstPrint();
