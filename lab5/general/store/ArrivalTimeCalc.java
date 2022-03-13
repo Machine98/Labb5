ae8a4534b0ae5f25abdb28cf4fa03928a37aaaa6
themooses <wilhelm.rauston@gmail.com>
2022-03-13 16:07:29 +0100

    ArrivalTimeCalc: Javadoc

--- lab5/general/store/ArrivalTimeCalc.java
+++ lab5/general/store/ArrivalTimeCalc.java
@@ -2,16 +2,39 @@ package lab5.general.store;
 
 import lab5.ExponentialRandomStream;
 
+/**
+ * Represents the part of the program that utilize ExponentialRandomStream to calculate and randomize the arrival time
+ *
+ * @author Calle Rautio
+ * @author Wilhelm Rauston
+ * @author Albin Sundström
+ * @author Eric Vikberg
+ */
+
 public class ArrivalTimeCalc {
     private double lambda;
     private long seed;
     private ExponentialRandomStream ArrivalTime;
+
+    /**
+     * Constructor
+     *
+     * @param lambda time divider
+     * @param seed the seed that the randomizers use to perform calculations
+     */
 
     public ArrivalTimeCalc(double lambda, long seed) {
         this.lambda = lambda;
         this.seed = seed;
         this.ArrivalTime = new ExponentialRandomStream(lambda, seed);
     }
+
+    /**
+     * Makes a double called newArrivalTime
+     *
+     * @return time it takes for customer to arrive
+     *
+     */
 
     public double newArrivalTime() {
         return ArrivalTime.next();
