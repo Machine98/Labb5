b9df89ec1341d489a2e0cc8c80894547b7c60386
FilthyTv <64787614+FilthyTv@users.noreply.github.com>
2022-03-13 15:18:04 +0100

    JavaDoc Event

--- lab5/general/Event.java
+++ lab5/general/Event.java
@@ -2,11 +2,28 @@ package lab5.general;
 
 import lab5.general.store.StoreState;
 
+/**
+ * Represents an abstract event that all other events will be built around
+ *
+ * @author Calle Rautio
+ * @author Wilhelm Rauston
+ * @author Albin Sundström
+ * @author Eric Vikberg
+ */
+
 public abstract class Event {
 
     private double time;
     private StoreState storeState;
     protected EventQueue eventQueue;
+
+    /**
+     * Constructor
+     *
+     * @param storeState - The stores state that keeps track of all the moving variables
+     * @param time - The time that it takes for an event to be run
+     * @param eventQueue - The event queue where it will add new events
+     */
 
     public Event(StoreState storeState, double time, EventQueue eventQueue) {
         this.storeState = storeState;
@@ -14,7 +31,17 @@ public abstract class Event {
         this.eventQueue = eventQueue;
     }
 
+    /**
+     * An abstract method for what the event will perform
+     */
+
     public abstract void performEvent();
+
+    /**
+     * Gets the events current time
+     *
+     * @return - Returns the time
+     */
 
     public double EventTime() {
         return time;
